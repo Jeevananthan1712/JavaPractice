e7790b8f000d0222129420f961bb41c0a710c98b
JEEVA <sjeevananthan1712@gmail.com>
2023-02-27 14:15:50 +0530

    Day4

--- src/com/BinarySearch/TwoSums.java
+++ src/com/BinarySearch/TwoSums.java
@@ -1,11 +1,10 @@
 package com.BinarySearch;
-
 import java.util.Arrays;
-
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
 public class TwoSums {
     public static void main(String[] args) {
-        int[] numbers = {2,7,11,15};
-        int target =13;
+        int[] numbers = {2,3,8,13,14};
+        int target = 11;
         System.out.println(Arrays.toString(twosum(numbers,target)));
     }
     static int[] twosum(int[] numbers,int target){
@@ -14,16 +13,16 @@ public class TwoSums {
         int[] ans = {-1,-1};
 
         while(start < end){
-            int mid = start +(end - start)/2;
             if(numbers[start] + numbers[end] == target){
-                ans[0] = start;
-                ans[1]= end;
+                ans[0] = start+1;
+                ans[1] = end+1;
+                return ans;
             }
-            if(target - numbers[mid] < numbers[mid]){
-                end = mid;
+            if(numbers[start] + numbers[end] > target ){
+                end--;
             }
-            if(target - numbers[mid] > numbers[mid]){
-                start = mid+1;
+            else if(numbers[start] + numbers[end] < target){
+                start++;
             }
         }
         return ans;
