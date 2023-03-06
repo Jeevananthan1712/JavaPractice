abce413f50ef097a44ef77e5e35ac4811f149fd4
JEEVA <sjeevananthan1712@gmail.com>
2023-03-06 23:52:12 +0530

    Day12

--- src/com/Arrays/SpiralMatrix.java
+++ src/com/Arrays/SpiralMatrix.java
@@ -1,2 +1,82 @@
-package com.Arrays;public class SpiralMatrix {
+package com.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+        };
+        System.out.println(sol.spiralOrder(matrix));
+    }
 }
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        int rowstart = 0;
+        int rowend = matrix.length-1;
+        int colstat = 0;
+        int colend = matrix[0].length-1;
+        List<Integer> list = new ArrayList<>();
+        while(rowstart <= rowend && colstat<=colend){
+
+            for(int i=colstat;i<=colend;i++)
+                list.add(matrix[rowstart][i]);
+            rowstart++;
+
+            for(int i=rowstart;i<=rowend;i++)
+                list.add(matrix[i][colend]);
+            colend--;
+
+            if(rowstart <= rowend){
+                for(int i=colend;i>=colstat;i--)
+                    list.add(matrix[rowend][i]);
+                rowend--;
+            }
+
+            if(colstat <= colend){
+                for(int i=rowend;i>=rowstart;i--)
+                    list.add(matrix[i][colstat]);
+                colstat++;
+            }
+
+        }
+        return list;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//{       rs {1,2,3,4}rs++                              {0,0,0,0
+//        rs {5,6,7,8}         i<col[0].length--         0      0
+//           {9,10,11,12}                                0     0
+//           {9,10,11,12}                                0,0,0,0
+
+//        re {13,14,15,16}}
+
+
+//Lets breakdown the code
+//1->2->3->4    i<mat[0].length;                                                4>2
+//rstart++
+//8->12->16     i=mat[0].length; i<mat.length;i++
+//mat[0].length--
+//15->14->13    i=mat[0].length; i>=0;i--
+//9->5          rend >= rstart
--- src/com/Arrays/Spirals2.java
+++ src/com/Arrays/Spirals2.java
@@ -1,2 +1,43 @@
-package com.Arrays;public class Spirals2 {
+package com.Arrays;
+
+import java.util.Arrays;
+
+public class Spirals2 {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(Arrays.deepToString(Buildmatrix(n)));
+    }
+    static int[][] Buildmatrix(int n){
+        int[][] matrix = new int[n][n];
+        int count= 1;
+        int rowstart = 0 ;
+        int rowend = n-1;
+        int colstart = 0;
+        int colend = n-1;
+        while(rowstart <= rowend && colstart<=colend) {
+            for (int i = colstart; i <= colend; i++)
+                matrix[rowstart][i] = count++;
+            rowstart++;
+
+            for(int i=rowstart;i<=rowend;i++)
+                matrix[i][colend] = count++;
+
+            colend--;
+
+            if(rowstart <= rowend){
+                for(int i=colend;i>=colstart;i--)
+                    matrix[rowend][i] = count++;
+                rowend--;
+            }
+
+            if(colstart <= colend){
+                for(int i=rowend;i>=rowstart;i--)
+                    matrix[i][colstart] = count++;
+                colstart++;
+            }
+        }
+
+
+        return matrix;
+    }
 }
