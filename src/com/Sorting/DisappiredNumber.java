0f439cc475728ea60d188c89a6b9fea8793e04a8
JEEVA <sjeevananthan1712@gmail.com>
2023-03-15 18:51:20 +0530

    Day21

--- src/com/Sorting/DisappiredNumber.java
+++ src/com/Sorting/DisappiredNumber.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class DisappiredNumber {
     public static void main(String[] args) {
         Solutn sol = new Solutn();
-        int[] arr = {4,3,2,7,8,2,3,1};
+        int[] arr = {4,3,2,7,8,3,1};
         System.out.println(sol.findDisappearedNumbers(arr));
 
     }
